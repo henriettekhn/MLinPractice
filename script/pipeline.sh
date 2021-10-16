37fd0d1a2fa57920fa383495f9aa946b3003b41f
huhle17 <Henriette.Uhlenbrock@gmx.de>
2021-10-16 15:25:19 +0200

    rename code to script due to merging

---
+++ script/pipeline.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# overall pipeline for the ML experiments
+
+echo "loading data"
+script/load_data.sh
+echo "preprocessing"
+script/preprocessing.sh
+echo "feature extraction"
+script/feature_extraction.sh
+echo "dimensionality reduction"
+script/dimensionality_reduction.sh
+echo "classification"
+script/classification.sh
---
+++ script/preprocessing.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# create directory if not yet existing
+mkdir -p data/preprocessing/split/
+
+# add labels
+echo "  creating labels"
+python -m script.preprocessing.create_labels data/raw/ data/preprocessing/labeled.csv
+
+# other preprocessing (removing punctuation etc.)
+echo "  general preprocessing"
+python -m script.preprocessing.run_preprocessing data/preprocessing/labeled.csv data/preprocessing/preprocessed.csv --punctuation -e data/preprocessing/pipeline.pickle
+
+# split the data set
+echo "  splitting the data set"
+python -m script.preprocessing.split_data data/preprocessing/preprocessed.csv data/preprocessing/split/ -s 42
